4c47edfef417952ab903c6014e0b4b7edcdf6bed
q3st1on <orlando.morrisjohnson@gmail.com>
2021-08-27 16:12:33 +0800

    bru

--- code/js/my-app/src/commands/cat.js
+++ code/js/my-app/src/commands/cat.js
@@ -4,8 +4,10 @@ export function cat (input, folder, path) {
     var index = 'a';
     for(let i = 0; i < folder.length; i++) {
         if(folder[i].id === (path+"/"+input)) {
-            if ((folder[i].folder === true) || (folder[i].x === false)) {
+            if (folder[i].folder === true) {
                 index = 'b';
+            } else if (folder[i].x === true) {
+                index = 'c';
             } else {
                 index = i;
                 break;
@@ -21,6 +23,9 @@ export function cat (input, folder, path) {
     } else if (index === 'b') {
         ret1 = "error";
         ret2 = "cat: "+input+": Is a directory";
+    } else if (index === 'c') {
+        ret1 = "error";
+        ret2 = "cat: "+input+": Is an executable";
     }
     return({ret1, ret2})
 }
